c93edff89c467f6298f9f170f03808c4f2f375f7
Leo Li <2907517155@qq.com>
2026-03-19 20:00:46 +0800

    Commit the second version of add_two_nums.py

--- add_two_nums.py
+++ add_two_nums.py
@@ -1,9 +1,10 @@
 # Add Two Numbers in Python
 # Author: Jinyu Li (Leo)
-# Using the "+" operation
+# Using user input
 
-a = 15
-b = 12
+# get input from user and convert them to float
+a = float(input("[Enter the first number]\n"))
+b = float(input("[Enter the second number]\n"))
 
 # add them together
 sum = a + b
